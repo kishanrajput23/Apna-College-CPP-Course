41124753fc690d3e5650766929568e2fa77ffcb7
Kishan <kishan.rai99693@gmail.com>
2021-06-18 13:48:43 +0530

    Finding Even & Odd Number

---
+++ Week 1/2.3 If-Else Statement/finding_EvenOdd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin>>n;
+    if (n%2==0) {
+        cout<<n<<" The number is Even"<<endl;
+    } 
+    else {
+        cout<<n<<" The number is Odd"<<endl;
+    }
+    return 0;
+}
