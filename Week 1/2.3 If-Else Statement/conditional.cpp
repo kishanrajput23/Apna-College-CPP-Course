9721d82c4cb90a12e261491ce0944ed4a62ddf0f
Kishan <kishan.rai99693@gmail.com>
2021-06-18 13:45:58 +0530

    conditional statements

---
+++ Week 1/2.3 If-Else Statement/conditional.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a;
+    int b;
+    cin>>a>>b;
+
+    if (a==b) {
+        cout<<"Both numbers are equal"<<endl;
+    }
+    else {
+        cout<<"Try Again!!"<<endl;
+    }
+    return 0;
+}
