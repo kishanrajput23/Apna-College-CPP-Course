befcbe57fe8c30cde851e1ad7217b7c4caa9d8b7
Kishan <kishan.rai99693@gmail.com>
2021-06-18 13:54:26 +0530

    Checking even & odd nos

---
+++ Week 1/2.3 If-Else Statement/program_to_check_number_is_even_or_odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter a number"<<endl;
+    cin>>n;
+    if (n%2==0) {
+        cout<<"The number is even"<<endl;
+    }
+    else {
+        cout<<"The number is odd"<<endl;
+    }
+    return 0;
+}
