5dede710daf3cb98dacbb2ef9465895c13e1c16c
Kishan <kishan.rai99693@gmail.com>
2021-07-07 19:09:23 +0530

    Hollow Rhombus Pattern

---
+++ week 2/4.2 Advanced Pattern Questions/pr_11_hollow_rhombus_pattern.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin>>n;
+
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=n-i; j++) {
+            cout<<" ";
+        }
+        for (int j=1; j<=n; j++) {
+            if (i==1 || i==n || j==1 || j==n) {
+                cout<<"* ";
+            }
+            else {
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
