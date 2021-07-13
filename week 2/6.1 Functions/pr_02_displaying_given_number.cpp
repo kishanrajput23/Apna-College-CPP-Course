650b3c6182b53f05034c1e84f003b1629cbf2917
Kishan <kishan.rai99693@gmail.com>
2021-07-13 20:40:43 +0530

    Displaying numbers

---
+++ week 2/6.1 Functions/pr_02_displaying_given_number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void display(int num) {
+    cout<<num<<endl;
+    return;
+}
+
+int main() {
+
+    int num;
+    cin>>num;
+
+    cout<<num<<endl;
+
+    return 0;
+}
