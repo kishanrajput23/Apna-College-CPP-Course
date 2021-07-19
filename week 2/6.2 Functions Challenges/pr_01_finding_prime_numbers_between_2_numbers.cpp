df7cb5b8354dd884d0f2255a4c2c72f8920ab045
Kishan <kishan.rai99693@gmail.com>
2021-07-19 19:44:39 +0530

    find prime nos b/w two no.

---
+++ week 2/6.2 Functions Challenges/pr_01_finding_prime_numbers_between_2_numbers.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+bool isPrime(int num) {
+    for (int i=2; i<=sqrt(num); i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int a;
+    int b;
+    cin>>a;
+    cin>>b;
+
+    for (int i=a; i<=b; i++) {
+        if (isPrime(i)) {
+            cout<<i<<endl;
+        }
+    }
+    return 0;
+}
